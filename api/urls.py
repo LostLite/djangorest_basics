8a4f0d9428cdc95c2ee1cc9fef35bac023130157
Wanderer <wanderer@Wanderers-MacBook-Pro.local>
2020-08-16 23:38:49 +0300

    views updates

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import get_articles, article_detail, ArticleAPIView
+from .views import get_articles, article_detail, ArticleAPIView, GenericAPIView
 
 urlpatterns = [
     # path('article/', get_articles , name='get-articles'), # uses function views
     path('article/', ArticleAPIView.as_view() , name='get-articles'),
     # path('article/<int:pk>', article_detail, name='article-details'), # uses function view
     path('article/<int:pk>', ArticleAPIView.as_view(), name='article-details'),
+    path('gen-article/', GenericAPIView.as_view() , name='gen-articles'),
+    path('gen-article/<int:id>', GenericAPIView.as_view() , name='gen-articles'),
 ]
--- api/views.py
+++ api/views.py
@@ -8,6 +8,37 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from  rest_framework import generics, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
+# this is a view that uses generic api view
+class GenericAPIView(generics.GenericAPIView, 
+    mixins.ListModelMixin, mixins.CreateModelMixin, 
+    mixins.UpdateModelMixin, mixins.RetrieveModelMixin, 
+    mixins.DestroyModelMixin):
+    serializer_class = ArticleModelSerializer
+    queryset = Article.objects.all()
+    lookup_field = 'id'
+    authentication_classes = [TokenAuthentication,]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, id=None):
+        
+        if id:
+            return self.retrieve(request,id)
+        
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+    def put(self, request, id):
+        return self.update(request,id)
+
+    def delete(self, request, id):
+        return self.destroy(request, id)
 
 
 # This is a class based view
